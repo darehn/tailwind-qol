7b28b45acc02a6761cdbb1177f300d2b57e7983b
daniel <daniel.rehn@nti.se>
2023-02-23 13:59:18 +0100

    added utility classes for animations, elevation and typography

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const size = require('./size.js');
+const elevation = require('./elevation.js');
+const animations = require('./animations.js');
+const typography = require('./typography.js');
 
 module.exports = {
 	size,
+	elevation,
+	animations,
+	typography,
 };
